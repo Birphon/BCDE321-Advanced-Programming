561b95020069f0ce43c02424b26ef54bf9cd7496
Jared <61985658+Birphon@users.noreply.github.com>
2022-06-01 10:25:48 +1200

    Delete ZIMP/Refactoring directory

--- ZIMP/Refactoring/main.py
+++
@@ -1,9 +0,0 @@
-import sys
-from Commands import Commands
-
-if __name__ == "__main__":
-    print("Number of command-line arguments: ", len(sys.argv))
-    print(sys.argv[0])
-    commands = Commands()
-    commands.cmdloop()
-
