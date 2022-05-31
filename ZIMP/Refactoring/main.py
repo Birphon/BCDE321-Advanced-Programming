a01568e54bc37e3f3f08563446ec2d0b2ea565aa
Jared <61985658+Birphon@users.noreply.github.com>
2022-06-01 10:26:42 +1200

    more refactoring done
    PyCharm is being dumb with GitHub :)

---
+++ ZIMP/Refactoring/main.py
@@ -0,0 +1,9 @@
+import sys
+from Commands import Commands
+
+if __name__ == "__main__":
+    print("Number of command-line arguments: ", len(sys.argv))
+    print(sys.argv[0])
+    commands = Commands()
+    commands.cmdloop()
+
