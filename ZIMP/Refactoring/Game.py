541070e7acfc218aaee6ca8d21dad0c297199833
Birphon <jaredireland1205@gmail.com>
2022-06-10 11:44:20 +1200

    Moved the Actions (from Actions.py) back into Game.py for now
    99 little bugs in the code.
    99 little bugs in the code.
    Take one down, patch it around,
    127 little bug in the code...

--- ZIMP/Refactoring/Game.py
+++ ZIMP/Refactoring/Game.py
@@ -307,240 +307,233 @@ class Game:
             if self.get_time == 11:
                 print("You have run out of time")
                 self.lose_game()
-            return
-        else:
-            print("Reshuffling The Deck")
-            self.load_dev_cards()
-            self.time += 1
+                return
+            else:
+                print("Reshuffling The Deck")
+                self.load_dev_cards()
+                self.time += 1
 
         dev_card = self.dev_cards[0]
         self.dev_cards.pop(0)
         event = dev_card.get_event_at_time(time)
         if event[0] == "Nothing":
             print("There is nothing in this room")
-            if len(self.load.chosen_tile.doors) == 1 and self.load.chosen_tile.name != "Foyer":
+            if len(self.chosen_tile.doors) == 1 and self.chosen_tile.name != "Foyer":
                 self.state = "Choosing Door"
-                  self.get_game()
-                  return
-              else:
-                  self.state = "Moving"
-                  self.get_game()
-              return
-          elif event[0] == "Health":
-               print("There might be something in this room")
-               self.player.add_health(event[1])
+                self.get_game()
+                return
+            else:
+                self.state = "Moving"
+                self.get_game()
+            return
+        elif event[0] == "Health":
+            print("There might be something in this room")
+            self.player.add_health(event[1])
 
-           if event[1] > 0:
-               print(f"You gained {event[1]} health")
-               self.state = "Moving"
-           elif event[1] < 0:
-               print(f"You lost {event[1]} health")
-               self.state = "Moving"
-               if self.player.get_health() <= 0:
-                   self.lose_game()
-                   return
-           elif event[1] == 0:
-               print("You didn't gain or lose any health")
-           if len(self.load.chosen_tile.doors) == 1 and self.load.chosen_tile.name != "Foyer":
-               self.state = "Choosing Door"
-           if self.get_current_tile().name == "Garden" or "Kitchen":
-               self.trigger_room_effect(self.get_current_tile().name)
-           else:
-               self.state = "Moving"
-               self.get_game()
-       elif event[0] == "Item":
-           if len(self.dev_cards) == 0:
-               if self.get_time == 11:
-                   print("You have run out of time")
-                   self.lose_game()
-                   return
-               else:
-                   print("Reshuffling The Deck")
-                   self.load_dev_cards()
-                   self.time += 1
-           next_card = self.dev_cards[0]
-           print(f"There is an item in this room: {next_card.get_item()}")
-           if len(self.player.get_items()) < 2:
-               self.dev_cards.pop(0)
-               self.player.add_item(next_card.get_item(), next_card.charges)
-               print(f"You picked up the {next_card.get_item()}")
-               if len(self.load.chosen_tile.doors) == 1 and self.load.chosen_tile.name != "Foyer":
-                   self.state = "Choosing Door"
-                   self.get_game()
-               else:
-                   self.state = "Moving"
-                   self.get_game()
-           else:
-               self.room_item = [next_card.get_item(), next_card.charges]
-               response = input(
-                   "You already have two items, do you want to drop one of them? (Y/N) ")
-               if response == "Y" or response == "y":
-                   self.state = "Swapping Item"
-               else:
-                   self.state = "Moving"
-                   self.room_item = None
-                   self.get_game()
-           if self.get_current_tile().name == "Garden" or "Kitchen":
-               self.trigger_room_effect(self.get_current_tile().name)
-       elif event[0] == "Zombies":
-           print(
-               f"There are {event[1]} zombies in this room, prepare to fight!")
-           self.current_zombies = int(event[1])
-           self.state = "Attacking"
+            if event[1] > 0:
+                print(f"You gained {event[1]} health")
+                self.state = "Moving"
+            elif event[1] < 0:
+                print(f"You lost {event[1]} health")
+                self.state = "Moving"
+                if self.player.get_health() <= 0:
+                    self.lose_game()
+                    return
+            elif event[1] == 0:
+                print("You didn't gain or lose any health")
+            if len(self.chosen_tile.doors) == 1 and self.chosen_tile.name != "Foyer":
+                self.state = "Choosing Door"
+            if self.get_current_tile().name == "Garden" or "Kitchen":
+                self.trigger_room_effect(self.get_current_tile().name)
+            else:
+                self.state = "Moving"
+                self.get_game()
+        elif event[0] == "Item":
+            if len(self.dev_cards) == 0:
+                if self.get_time == 11:
+                    print("You have run out of time")
+                    self.lose_game()
+                    return
+                else:
+                    print("Reshuffling The Deck")
+                    self.load_dev_cards()
+                    self.time += 1
+            next_card = self.dev_cards[0]
+            print(f"There is an item in this room: {next_card.get_item()}")
+            if len(self.player.get_items()) < 2:
+                self.dev_cards.pop(0)
+                self.player.add_item(next_card.get_item(), next_card.charges)
+                print(f"You picked up the {next_card.get_item()}")
+                if len(self.chosen_tile.doors) == 1 and self.chosen_tile.name != "Foyer":
+                    self.state = "Choosing Door"
+                    self.get_game()
+                else:
+                    self.state = "Moving"
+                    self.get_game()
+            else:
+                self.room_item = [next_card.get_item(), next_card.charges]
+                response = input("You already have two items, do you want to drop one of them? (Y/N) ")
+                if response == "Y" or response == "y":
+                    self.state = "Swapping Item"
+                else:
+                    self.state = "Moving"
+                    self.room_item = None
+                    self.get_game()
+            if self.get_current_tile().name == "Garden" or "Kitchen":
+                self.trigger_room_effect(self.get_current_tile().name)
+        elif event[0] == "Zombies":
+            print(f"There are {event[1]} zombies in this room, prepare to fight!")
+            self.current_zombies = int(event[1])
+            self.state = "Attacking"
 
     def trigger_attack(self, *item):
-       player_attack = self.player.get_attack()
-       zombies = self.current_zombies
-       if len(item) == 2:
-           if "Oil" in item and "Candle" in item:
-               print(
-                   "You used the oil and the candle to attack the zombies, it kills all of them")
-               self.drop_item("Oil")
-               self.state = "Moving"
-               return
-           elif "Gasoline" in item and "Candle" in item:
-               print(
-                   "You used the gasoline and the candle to attack the zombies, it kills all of them")
-               self.drop_item("Gasoline")
-               self.state = "Moving"
-               return
-           elif "Gasoline" in item and "Chainsaw" in item:
-               chainsaw_charge = self.player.get_item_charges("Chainsaw")
-               self.player.set_item_charges("Chainsaw", chainsaw_charge + 2)
-               player_attack += 3
-               self.drop_item("Gasoline")
-               self.player.use_item_charge("Chainsaw")
-           else:
-               print("These items cannot be used together, try again")
-               return
-       elif len(item) == 1:
-           if "Machete" in item:
-               player_attack += 2
-           elif "Chainsaw" in item:
-               if self.player.get_item_charges("Chainsaw") > 0:
-                   player_attack += 3
-                   self.player.use_item_charge("Chainsaw")
-               else:
-                   print("This item has no charges left")
-           elif "Golf Club" in item or "Grisly Femur" in item or "Board With Nails" in item:
-               player_attack += 1
-           elif "Can of Soda" in item:
-               self.player.add_health(2)
-               self.drop_item("Can of Soda")
-               print("Used Can of Soda, gained 2 health")
-               return
-           elif "Oil" in item:
-               self.trigger_run(0)
-               return
-         else:
-              print("You cannot use this item right now, try again")
-               return
+        player_attack = self.player.get_attack()
+        zombies = self.current_zombies
+        if len(item) == 2:
+            if "Oil" in item and "Candle" in item:
+                print("You used the oil and the candle to attack the zombies, it kills all of them")
+                self.drop_item("Oil")
+                self.state = "Moving"
+                return
+            elif "Gasoline" in item and "Candle" in item:
+                print("You used the gasoline and the candle to attack the zombies, it kills all of them")
+                self.drop_item("Gasoline")
+                self.state = "Moving"
+                return
+            elif "Gasoline" in item and "Chainsaw" in item:
+                chainsaw_charge = self.player.get_item_charges("Chainsaw")
+                self.player.set_item_charges("Chainsaw", chainsaw_charge + 2)
+                player_attack += 3
+                self.drop_item("Gasoline")
+                self.player.use_item_charge("Chainsaw")
+            else:
+                print("These items cannot be used together, try again")
+                return
+        elif len(item) == 1:
+            if "Machete" in item:
+                player_attack += 2
+            elif "Chainsaw" in item:
+                if self.player.get_item_charges("Chainsaw") > 0:
+                    player_attack += 3
+                    self.player.use_item_charge("Chainsaw")
+                else:
+                    print("This item has no charges left")
+            elif "Golf Club" in item or "Grisly Femur" in item or "Board With Nails" in item:
+                player_attack += 1
+            elif "Can of Soda" in item:
+                self.player.add_health(2)
+                self.drop_item("Can of Soda")
+                print("Used Can of Soda, gained 2 health")
+                return
+            elif "Oil" in item:
+                self.trigger_run(0)
+                return
+            else:
+                print("You cannot use this item right now, try again")
+                return
 
-       damage = zombies - player_attack
-       if damage < 0:
-           damage = 0
-       print(f"You attacked the zombies, you lost {damage} health")
-       self.can_cower = True
-       self.player.add_health(-damage)
-       if self.player.get_health() <= 0:
-           self.lose_game()
-           return
-       else:
-           self.current_zombies = 0
-           if self.get_current_tile().name == "Garden" or "Kitchen":
-               self.trigger_room_effect(self.get_current_tile().name)
-           self.state = "Moving"
+        damage = zombies - player_attack
+        if damage < 0:
+            damage = 0
+        print(f"You attacked the zombies, you lost {damage} health")
+        self.can_cower = True
+        self.player.add_health(-damage)
+        if self.player.get_health() <= 0:
+            self.lose_game()
+            return
+        else:
+            self.current_zombies = 0
+            if self.get_current_tile().name == "Garden" or "Kitchen":
+                self.trigger_room_effect(self.get_current_tile().name)
+            self.state = "Moving"
 
     def trigger_run(self, direction, health_lost=-1):
-       self.state = "Running"
-       self.select_move(direction)
-       if self.state == "Moving":
-           self.player.add_health(health_lost)
-           print(
-               f"You run away from the zombies, and lose {health_lost} health")
-           self.can_cower = True
-           if self.get_current_tile().name == "Garden" or "Kitchen":
-               self.trigger_room_effect(self.get_current_tile().name)
-       else:
-           self.state = "Attacking"
+        self.state = "Running"
+        self.select_move(direction)
+        if self.state == "Moving":
+            self.player.add_health(health_lost)
+            print(f"You run away from the zombies, and lose {health_lost} health")
+            self.can_cower = True
+            if self.get_current_tile().name == "Garden" or "Kitchen":
+                self.trigger_room_effect(self.get_current_tile().name)
+        else:
+            self.state = "Attacking"
 
     def trigger_room_effect(self, room_name):
-       if room_name == "Garden":
-           self.player.add_health(1)
-           print(
-               f"After ending your turn in the {room_name} you have gained one health")
-           self.state = "Moving"
-       if room_name == "Kitchen":
-           self.player.add_health(1)
-           print(
-               f"After ending your turn in the {room_name} you have gained one health")
-           self.state = "Moving"
+        if room_name == "Garden":
+            self.player.add_health(1)
+            print(f"After ending your turn in the {room_name} you have gained one health")
+            self.state = "Moving"
+        if room_name == "Kitchen":
+            self.player.add_health(1)
+            print(f"After ending your turn in the {room_name} you have gained one health")
+            self.state = "Moving"
 
     def trigger_cower(self):
-       if self.can_cower:
-           self.player.add_health(3)
-           self.dev_cards.pop(0)
-           self.state = "Moving"
-           print("You cower in fear, gaining 3 health, but lose time with the dev card")
-       else:
-           return print("Cannot cower during a zombie door attack")
+        if self.can_cower:
+            self.player.add_health(3)
+            self.dev_cards.pop(0)
+            self.state = "Moving"
+            print("You cower in fear, gaining 3 health, but lose time with the dev card")
+        else:
+            return print("Cannot cower during a zombie door attack")
 
     def drop_item(self, old_item):
-       for item in self.player.get_items():
-           if item[0] == old_item:
-               self.player.remove_item(item)
-               print(f"You dropped the {old_item}")
-               self.state = "Moving"
-               return
-       print("That item is not in your inventory")
+        for item in self.player.get_items():
+            if item[0] == old_item:
+                self.player.remove_item(item)
+                print(f"You dropped the {old_item}")
+                self.state = "Moving"
+                return
+        print("That item is not in your inventory")
 
     def use_item(self, *item):
-       if "Can of Soda" in item:
-           self.player.add_health(2)
-           self.drop_item("Can of Soda")
-           print("Used Can of Soda, gained 2 health")
-       elif "Gasoline" in item and "Chainsaw" in item:
-           chainsaw_charge = self.player.get_item_charges("Chainsaw")
-           self.player.set_item_charges("Chainsaw", chainsaw_charge + 2)
-           self.drop_item("Gasoline")
-       else:
-           print("These items cannot be used right now")
-           return
+        if "Can of Soda" in item:
+            self.player.add_health(2)
+            self.drop_item("Can of Soda")
+            print("Used Can of Soda, gained 2 health")
+        elif "Gasoline" in item and "Chainsaw" in item:
+            chainsaw_charge = self.player.get_item_charges("Chainsaw")
+            self.player.set_item_charges("Chainsaw", chainsaw_charge + 2)
+            self.drop_item("Gasoline")
+        else:
+            print("These items cannot be used right now")
+            return
 
     def choose_door(self, direction):
-       if direction in self.load.chosen_tile.doors:
-           print("Choose a NEW door not an existing one")
-           return False
-       else:
-           self.load.chosen_tile.doors.append(direction)
-           self.current_zombies = 3
-           print(f"{self.current_zombies} Zombies have appeared, prepare for battle. Use the attack command to"
-                 f" fight or the run command to flee")
-           self.state = "Attacking"
+        if direction in self.chosen_tile.doors:
+            print("Choose a NEW door not an existing one")
+            return False
+        else:
+            self.chosen_tile.doors.append(direction)
+            self.current_zombies = 3
+            print(f"{self.current_zombies} Zombies have appeared, prepare for battle. Use the attack command to"
+                  f" fight or the run command to flee")
+            self.state = "Attacking"
 
     def search_for_totem(self):
-       if self.get_current_tile().name == "Evil Temple":
-           if self.player.has_totem:
-               print("player already has the totem")
-               return
-           else:
-               self.trigger_dev_card(self.time)
-               self.player.found_totem()
-       else:
-           print("You cannot search for a totem in this room")
+        if self.get_current_tile().name == "Evil Temple":
+            if self.player.has_totem:
+                print("player already has the totem")
+                return
+            else:
+                self.trigger_dev_card(self.time)
+                self.player.found_totem()
+        else:
+            print("You cannot search for a totem in this room")
 
     def bury_totem(self):
-       if self.get_current_tile().name == "Graveyard":
-           if self.player.has_totem:
-               self.trigger_dev_card(self.time)
-               if self.player.health != 0:
-                   print("You Won")
-                   self.state = "Game Over"
-       else:
-           print("Cannot bury totem here")
+        if self.get_current_tile().name == "Graveyard":
+            if self.player.has_totem:
+                self.trigger_dev_card(self.time)
+                if self.player.health != 0:
+                    print("You Won")
+                    self.state = "Game Over"
+        else:
+            print("Cannot bury totem here")
 
     def check_for_dead_player(self):
-       if self.player.health <= 0:
-           return True
-       else:
-           return False
+        if self.player.health <= 0:
+            return True
+        else:
+            return False
